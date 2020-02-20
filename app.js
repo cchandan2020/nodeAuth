de7a530b5b96cb8cb85f6e8d6dae92248b712c04
Chandan Chowdhury <chandan.chowdhury@gmail.com>
2020-02-20 08:02:17 +0000

    Added bcypt, updated several files

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-var expressValidator = require ('express-validator');
-//var expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
-var localStrategy= require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
-
-var flash=require('connect-flash');
-var mongo= require('mongodb');
-var mongoose= require('mongoose');
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -25,70 +25,88 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//handle file uploads
-//app.use(multer({dest:'./uploads'}));
-
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//Hndle sessions
+// Handle Sessions
 app.use(session({
-  secret: 'secret',
-  saveUninitialized:true,
-  resave:true
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
 }));
 
-//Passport
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//validator 
-// app.use(expressValidator({
-//   errorFormatter: function(param,msg,value){
-//     var namespace = param.split('.'),
-//     root = namespace.shift(),
-//     formParam = root;
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-//   while(namespace.length){
-//   formParam += '[' + namespace.shift() +']';
-//   }
-//   return {
-//     param: formParam,
-//     msg: msg,
-//     value: value
-//   };
-// }
-// }));
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
-// adding another middleware - connect-flash
-app.use(require('connect-flash')());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
